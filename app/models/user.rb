df7e37777d4131c92bf3daf9c298c08585548e5d
belly <abdelrhmanalaa39@gmail.com>
2016-12-14 05:10:00 +0000

    add user&email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ActiveRecord::Base
+  validates :username , presence: true , uniqueness: {case_sensitive: false},
+  length: {minimum: 4 , maximum: 10}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email , presence: true , uniqueness: { case_sensitive: false } , length: {minimum: 5 , maximum: 15 },
+  format: { with: VALID_EMAIL_REGEX }
+  
   
 end
