bfaa20f40aed086f89f984b237ad9dd3fb3b8e5c
belly <abdelrhmanalaa39@gmail.com>
2016-12-16 23:55:59 +0000

    add user admin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_article, only: [:edit, :update, :show]
-  before_action :require_same_user, only: [:edit , :update]
+  before_action :require_same_user, only: [:edit , :update, :destroy]
+  before_action :require_admin, only: [:destroy]
   def new
     @user = User.new
   end
@@ -37,6 +38,13 @@ class UsersController < ApplicationController
     
   end
   
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    flash[:danger] = "User and articles created by this user have been deleted"
+    redirect_to users_path
+  end
+  
   def show
    
     @user_articles = @user.articles.paginate(page: params[:page],per_page: 3)
@@ -50,10 +58,16 @@ class UsersController < ApplicationController
     params.require(:user).permit(:username , :email , :password)
   end
   def require_same_user
-    if  current_user != @user
+    if  current_user != @user and !current_user.admin?
       flash[:danger] = "you only can edit your profile"
       redirect_to root_path
       
+    end
+  end
+  def require_admin
+    if logged_in? and !current_user.admin?
+      flash[:danger] = "only admin can delete users"
+      redirect_to root_path
     end
   end
   
