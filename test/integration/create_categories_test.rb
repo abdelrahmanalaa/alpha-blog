ab7bf058ded184e4afd11791c68eb145a21e56ad
belly <abdelrhmanalaa39@gmail.com>
2016-12-26 03:58:00 +0000

    update test and navbar to create new category

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class CreateCategoriesTest < ActionDispatch::IntegrationTest
+  
+  def setup
+    @user = User.create(username: "belly", email: "abdelrhman@belly.com", password: "password", admin: true)
+  end
+  
   test "get new category form and create category" do
+    sign_in_as(@user, "password")
     get new_category_path
     assert_template 'categories/new'
     assert_difference 'Category.count' , 1 do
@@ -12,6 +18,7 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
   end
   
   test 'invalid category submission results in failure' do
+    sign_in_as(@user, "password")
   get new_category_path
     assert_template 'categories/new'
     assert_no_difference 'Category.count'  do
@@ -21,4 +28,5 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
     assert_select 'h2.panel-title'
     assert_select 'div.panel-body'
   end
+  
 end
