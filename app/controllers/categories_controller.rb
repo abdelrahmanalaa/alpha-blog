bfe04f3719137fed7c4b08edb6a02133a8b55ad8
belly <abdelrhmanalaa39@gmail.com>
2016-12-30 14:27:39 +0000

    add edit to categories for admin users

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -28,6 +28,22 @@ def show
 
 end
 
+def edit
+  @category = Category.find(params[:id])
+end
+
+def update
+  @category = Category.find(params[:id])
+  if @category.update(category_params)
+  flash[:success] = "category was successfully updated"
+  redirect_to category_path(@category)
+  else
+    render 'edit'
+  end
+  
+
+end
+
 private
 
 def category_params 
